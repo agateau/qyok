c839ce718fb331b7ca8a2c13bd3887454aab30a7
Aurélien Gâteau <agateau@kde.org>
2011-09-14 17:58:11 +0200

    Show multiple days

--- logdialog.py
+++ logdialog.py
@@ -20,31 +20,51 @@ body {
     font-family: sans-serif;
 }
 h1 {
+    font-size: 140%;
+    text-align: center;
+}
+h2 {
     font-size: 120%;
     margin: 0;
     padding: 0;
     padding-top: 1em;
 }
-h1:first-child {
+h2:first-child {
     padding-top: 0;
 }
 ul {
     margin: 0;
 }
+.day {
+    border-top: 1px dotted silver;
+}
+.day:first-child {
+    border-top: none;
+}
 </style>
 </head>
 <body>
-{% for project in projects %}
-    <h1>{{ project }}</h1>
-    <ul>
-    {% for task in taskDict[project] %}
-        <li>{{ task }}</li>
+{% for date, projects in dct|dictsort %}
+    <div class='day'>
+    <h1>{{ date.strftime("%A %d %B %Y") }}</h1>
+    {% for project, tasks in projects|dictsort %}
+        <h2>{{ project }}</h2>
+        <ul>
+        {% for task in tasks %}
+            <li>{{ task }}</li>
+        {% endfor %}
+        </ul>
     {% endfor %}
-    </ul>
+    </div>
 {% endfor %}
+</tr>
+</table>
 </body>
 </html>
 """)
+
+def datetimeFromQDate(qdate):
+    return datetime(qdate.year(), qdate.month(), qdate.day())
 
 class LogDialog(QDialog):
     def __init__(self):
@@ -52,30 +72,58 @@ class LogDialog(QDialog):
         self.ui = Ui_LogDialog()
         self.ui.setupUi(self)
 
-        QObject.connect(self.ui.dateEdit, SIGNAL("dateChanged(QDate)"), self.updateView)
-        self.ui.dateEdit.setFocus()
-        self.ui.dateEdit.setDate(QDate.currentDate())
+        self.ui.fromDateEdit.setFocus()
+        self.ui.fromDateEdit.setDate(QDate.currentDate())
+        self.ui.toDateEdit.setDate(QDate.currentDate())
 
         self.ui.webView.settings().setDefaultTextEncoding("utf-8")
 
+        QObject.connect(self.ui.fromDateEdit, SIGNAL("dateChanged(QDate)"), self.updateView)
+        QObject.connect(self.ui.toDateEdit, SIGNAL("dateChanged(QDate)"), self.updateView)
+
+        self.updateView()
+
     def updateView(self):
-        qdate = self.ui.dateEdit.date()
-        minDate = datetime(qdate.year(), qdate.month(), qdate.day())
-        maxDate = minDate + timedelta(1)
-        lst = Task.select(AND( \
+        minDate = datetimeFromQDate(self.ui.fromDateEdit.date())
+        maxDate = datetimeFromQDate(self.ui.toDateEdit.date())
+        if maxDate < minDate:
+            minDate, maxDate = maxDate, minDate
+
+        maxDate += timedelta(1)
+        tasks = Task.select(AND( \
             Task.q.status == "done", \
             Task.q.doneDate >= minDate, \
             Task.q.doneDate < maxDate, \
             ))
 
-        taskDict = {}
-        for task in lst:
+        # Create a tree-like struct with a dict of the form:
+        # {date => {project => [tasks]}
+        # Ie:
+        # date1
+        # - project1
+        #   - foo
+        #   - bar
+        # - project2
+        #   - baz
+        dct = {}
+        for task in tasks:
+            date = task.doneDate.date()
             project = task.project.name
-            if not project in taskDict:
-                taskDict[project] = []
-            taskDict[project].append(task.title)
+            title = task.title
 
-        projects = taskDict.keys()
-        projects.sort()
-        html = TEMPLATE.render(projects=projects, taskDict=taskDict)
+            if not date in dct:
+                dct[date] = {}
+            if not project in dct[date]:
+                dct[date][project] = []
+
+            dct[date][project].append(title)
+
+        # Add missing days
+        date = minDate.date()
+        while date < maxDate.date():
+            if not date in dct:
+                dct[date] = {}
+            date += timedelta(1)
+
+        html = TEMPLATE.render(dct=dct)
         self.ui.webView.setHtml(html)
