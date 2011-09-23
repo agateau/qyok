45ea7c7f73ccdae568fb2ff835f091a29df240af
Aurélien Gâteau <agateau@kde.org>
2011-09-23 11:37:52 +0200

    Mass rework: add query selector

--- logdialog.py
+++ logdialog.py
@@ -71,7 +71,7 @@ li:last-child {
     border-bottom-right-radius: 4px;
     border-bottom: 1px solid #666;
 }
-.day {
+.group1 {
     margin: 6px;
     padding: 6px;
 }
@@ -83,18 +83,18 @@ li:last-child {
 </style>
 </head>
 <body>
-{% for date, projects in dct|dictsort|reverse %}
-    <div class='day'>
-    <h1>{{ date|formatdate }}</h1>
-    {% for project, tasks in projects|dictsort %}
-        <h2>{{ project|e }}</h2>
+{% for grp1, lst in lst|groupby('grp1') %}
+    <div class='group1'>
+    <h1>{{ fmt1(grp1) }}</h1>
+    {% for grp2, lst in lst|groupby('grp2') %}
+        <h2>{{ grp2|e }}</h2>
         <ul>
-        {% for task in tasks %}
+        {% for item in lst %}
             <li>
-            {% if task.dueDate %}
-                <span class='due-date'>Due: {{ task.dueDate|formatdate }}</span>
+            {% if item.task.dueDate %}
+                <span class='due-date'>Due: {{ item.task.dueDate|formatdate }}</span>
             {% endif %}
-            {{ task.title|e }}
+            {{ item.task.title|e }}
             </li>
         {% endfor %}
         </ul>
@@ -108,6 +108,10 @@ li:last-child {
 def datetimeFromQDate(qdate):
     return datetime(qdate.year(), qdate.month(), qdate.day())
 
+QUERY_DUE = 0
+QUERY_PROJECT = 1
+QUERY_DONE = 2
+
 class LogDialog(QDialog):
     def __init__(self):
         super(LogDialog, self).__init__()
@@ -115,11 +119,9 @@ class LogDialog(QDialog):
         self.ui.setupUi(self)
         self.layout().setMargin(0)
 
-        self.ui.fromDateEdit.setFocus()
-        self.ui.fromDateEdit.setDate(QDate.currentDate())
+        self.ui.fromDateEdit.setDate(QDate.currentDate().addDays(-7))
         self.ui.toDateEdit.setDate(QDate.currentDate())
-
-        self.setupDueComboBox()
+        self.ui.dueDateEdit.setDate(QDate.currentDate())
 
         self.ui.webView.settings().setDefaultTextEncoding("utf-8")
 
@@ -127,28 +129,22 @@ class LogDialog(QDialog):
                 (self.ui.fromDateEdit, "dateChanged(QDate)"),
                 (self.ui.toDateEdit, "dateChanged(QDate)"),
                 (self.ui.projectLineEdit, "textChanged(QString)"),
-                (self.ui.newOrStartedRadioButton, "toggled(bool)"),
-                (self.ui.doneRadioButton, "toggled(bool)"),
-                (self.ui.dueComboBox, "activated(int)"),
+                (self.ui.queryListWidget, "itemSelectionChanged()"),
             ]:
             QObject.connect(obj, SIGNAL(signal), self.updateView)
 
+        QObject.connect(self.ui.queryListWidget, SIGNAL("itemSelectionChanged()"), self.updateFilterWidgets)
+        self.updateFilterWidgets()
         self.updateView()
 
-    def setupDueComboBox(self):
-        self.ui.dueComboBox.addItem(QString())
-
-        date = QDate.currentDate()
-        self.ui.dueComboBox.addItem(self.tr("Today"), date)
-
-        date = date.addDays(7 - date.dayOfWeek())
-        self.ui.dueComboBox.addItem(self.tr("End of Week"), date)
-
-        date = QDate(date.year(), date.month(), date.daysInMonth())
-        self.ui.dueComboBox.addItem(self.tr("End of Month"), date)
+    def updateFilterWidgets(self):
+        queryType = self.ui.queryListWidget.currentRow()
+        self.ui.dueWidget.setVisible(queryType == QUERY_DUE)
+        self.ui.doneWidget.setVisible(queryType == QUERY_DONE)
 
     def updateView(self):
         filters = []
+        queryType = self.ui.queryListWidget.currentRow()
 
         # Project
         wantedProject = self.ui.projectLineEdit.text()
@@ -161,12 +157,7 @@ class LogDialog(QDialog):
 
         # Status
         statusFilters = []
-        groupByDoneDate = False
-        if self.ui.newOrStartedRadioButton.isChecked():
-            statusFilters.append(Task.q.status == "new")
-            statusFilters.append(Task.q.status == "started")
-        if self.ui.doneRadioButton.isChecked():
-            groupByDoneDate = True
+        if queryType == QUERY_DONE:
             minDate = datetimeFromQDate(self.ui.fromDateEdit.date())
             maxDate = datetimeFromQDate(self.ui.toDateEdit.date())
             if maxDate < minDate:
@@ -179,41 +170,46 @@ class LogDialog(QDialog):
                 Task.q.doneDate < maxDate \
                 )
             statusFilters.append(doneFilter)
+        else:
+            statusFilters.append(Task.q.status == "new")
+            statusFilters.append(Task.q.status == "started")
 
         filters.append(OR(*statusFilters))
 
         # Due date
-        variant = self.ui.dueComboBox.itemData(self.ui.dueComboBox.currentIndex())
-        if variant.isValid():
-            date = datetimeFromQDate(variant.toDate()) + timedelta(1)
-            filters.append(Task.q.dueDate <= date)
+        if queryType == QUERY_DUE:
+            dueDate = datetimeFromQDate(self.ui.dueDateEdit.date()) + timedelta(1)
+            filters.append(Task.q.dueDate < dueDate)
 
         tasks = Task.select(AND(*filters))
 
-
-        # Create a tree-like struct with a dict of the form:
-        # {date => {project => [tasks]}
-        # Ie:
-        # date1
-        # - project1
-        #   - foo
-        #   - bar
-        # - project2
-        #   - baz
-        dct = {}
+        lst = []
+        class Item(object):
+            __slots__ = ["task", "grp1", "grp2"]
         for task in tasks:
-            if groupByDoneDate:
-                date = task.doneDate.date()
+            item = Item()
+            item.task = task
+            if queryType == QUERY_DUE:
+                item.grp1 = task.dueDate.date()
+                item.grp2 = task.project.name
+            elif queryType == QUERY_PROJECT:
+                item.grp1 = task.project.name
+                item.grp2 = ""
+            elif queryType == QUERY_DONE:
+                item.grp1 = task.doneDate.date()
+                item.grp2 = task.project.name
             else:
-                date = task.creationDate.date()
-            project = task.project.name
+                raise Exception()
+            lst.append(item)
 
-            if not date in dct:
-                dct[date] = {}
-            if not project in dct[date]:
-                dct[date][project] = []
+        if queryType == QUERY_DUE:
+            fmt1 = formatdate
+        elif queryType == QUERY_PROJECT:
+            fmt1 = lambda x: x
+        elif queryType == QUERY_DONE:
+            fmt1 = formatdate
+        else:
+            raise Exception()
 
-            dct[date][project].append(task)
-
-        html = TEMPLATE.render(dct=dct)
+        html = TEMPLATE.render(lst=lst, fmt1=fmt1)
         self.ui.webView.setHtml(html)
