f9db72bc9de9376653845807ba5697d580f0d124
Aurélien Gâteau <agateau@kde.org>
2011-10-05 00:16:54 +0200

    Refactor query system

--- logdialog.py
+++ logdialog.py
@@ -6,64 +6,13 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 
-from sqlobject import AND, OR, LIKE, IN
-from sqlobject.sqlbuilder import Select
-
 from jinja2 import Environment, FileSystemLoader
-
-from yokadi.db import Task, Project
 
 from ui_logdialog import Ui_LogDialog
 from addtaskdialog import AddTaskDialog
 from qydateutils import datetimeFromQDate, qdateFromDatetime
 
-def formatDate(date):
-    """
-    strftime may return a string with accent ("August" in fr is "Août" for
-    example), so we need to turn it into proper unicode.
-    """
-    return unicode(date.strftime("%A %d %B %Y"), "utf-8")
-
-def __tr(txt):
-    return QApplication.translate("", txt)
-
-def formatDueDate(dueDate):
-    today = datetime.now()
-    remaining = (dueDate.date() - today.date()).days
-    if remaining < 0:
-        txt = __tr("%1 days overdue").arg(-remaining)
-    elif remaining == 0:
-        txt = __tr("Due today")
-    elif remaining == 1:
-        txt = __tr("Due tomorrow")
-    elif remaining < 7:
-        txt = __tr("%1 days left").arg(remaining)
-    else:
-        txt = __tr("%1 weeks left").arg(remaining / 7)
-    return unicode(txt)
-
-def dueDateCssClass(task):
-    done = task.status == "done"
-    if done:
-        refDate = task.doneDate
-    else:
-        refDate = datetime.now()
-    remaining = (task.dueDate.date() - refDate.date()).days
-
-    if done:
-        if remaining < 0:
-            return "due-date-overdue"
-        else:
-            return ""
-
-    if remaining < 0:
-        return "due-date-overdue"
-    elif remaining == 0:
-        return "due-date-today"
-    elif remaining < 7:
-        return "due-date-week"
-    else:
-        return ""
+import queries
 
 QUERY_DUE = 0
 QUERY_PROJECT = 1
@@ -94,20 +43,20 @@ class LogDialog(QDialog):
                 (self.ui.projectLineEdit, "textChanged(QString)"),
                 (self.ui.queryListWidget, "itemSelectionChanged()"),
             ]:
-            QObject.connect(obj, SIGNAL(signal), self.updateView)
+            QObject.connect(obj, SIGNAL(signal), self.updateQuery)
 
         QObject.connect(self.ui.queryListWidget, SIGNAL("itemSelectionChanged()"), self.updateFilterWidgets)
 
         QObject.connect(self.ui.webView, SIGNAL("linkClicked(const QUrl&)"), self.openUrl)
 
         self.updateFilterWidgets()
-        self.updateView()
+        self.updateQuery()
 
     def setupJinjaEnv(self):
         self.jinjaEnv = Environment()
-        self.jinjaEnv.filters["dueDateCssClass"] = dueDateCssClass
-        self.jinjaEnv.filters["formatDate"] = formatDate
-        self.jinjaEnv.filters["formatDueDate"] = formatDueDate
+        self.jinjaEnv.filters["dueDateCssClass"] = queries.dueDateCssClass
+        self.jinjaEnv.filters["formatDate"] = queries.formatDate
+        self.jinjaEnv.filters["formatDueDate"] = queries.formatDueDate
 
         tmplDir = os.path.join(self.dataDir, "templates")
         self.jinjaEnv.loader = FileSystemLoader(tmplDir)
@@ -118,20 +67,28 @@ class LogDialog(QDialog):
         self.ui.doneWidget.setVisible(queryType == QUERY_DONE)
 
     def updateView(self):
-        filters = []
+        args = self.query.run()
+        tmpl = self.jinjaEnv.get_template(self.query.templateName)
+        html = tmpl.render(args)
+        baseUrl = QUrl.fromLocalFile(os.path.join(self.dataDir, "static/"))
+        self.ui.webView.setHtml(html, baseUrl)
+        self.ui.webView.page().mainFrame().addToJavaScriptWindowObject("qtWindow", self)
+
+    def updateQuery(self):
         queryType = self.ui.queryListWidget.currentRow()
+        queryClasses = {
+            QUERY_DUE: queries.DueQuery,
+            QUERY_PROJECT: queries.ProjectQuery,
+            QUERY_DONE: queries.DoneQuery,
+            }
+        self.query = queryClasses[queryType]()
 
         # Project
-        wantedProject = self.ui.projectLineEdit.text()
-        if not wantedProject.isEmpty():
-            filters.append(
-                IN(
-                    Task.q.project,
-                    Select(Project.q.id, LIKE(Project.q.name, "%" + unicode(wantedProject) + "%"))
-                ))
+        projectName = self.ui.projectLineEdit.text()
+        if not projectName.isEmpty():
+            self.query.projectName = unicode(projectName)
 
         # Status
-        statusFilters = []
         if queryType == QUERY_DONE:
             minDate = datetimeFromQDate(self.ui.fromDateEdit.date())
             maxDate = datetimeFromQDate(self.ui.toDateEdit.date())
@@ -139,58 +96,14 @@ class LogDialog(QDialog):
                 minDate, maxDate = maxDate, minDate
 
             maxDate += timedelta(1)
-            doneFilter = AND( \
-                Task.q.status == "done", \
-                Task.q.doneDate >= minDate, \
-                Task.q.doneDate < maxDate \
-                )
-            statusFilters.append(doneFilter)
-        else:
-            statusFilters.append(Task.q.status == "new")
-            statusFilters.append(Task.q.status == "started")
-
-        filters.append(OR(*statusFilters))
+            self.query.minDate = minDate
+            self.query.maxDate = maxDate
 
         # Due date
         if queryType == QUERY_DUE:
             dueDate = datetimeFromQDate(self.ui.dueDateEdit.date()) + timedelta(1)
-            filters.append(Task.q.dueDate < dueDate)
-
-        tasks = Task.select(AND(*filters))
-
-        lst = []
-        class Item(object):
-            __slots__ = ["task", "grp1", "grp2"]
-        for task in tasks:
-            item = Item()
-            item.task = task
-            if queryType == QUERY_DUE:
-                item.grp1 = task.dueDate.date()
-                item.grp2 = task.project.name
-            elif queryType == QUERY_PROJECT:
-                item.grp1 = task.project.name
-                item.grp2 = ""
-            elif queryType == QUERY_DONE:
-                item.grp1 = task.doneDate.date()
-                item.grp2 = task.project.name
-            else:
-                raise Exception()
-            lst.append(item)
-
-        if queryType == QUERY_DUE:
-            fmt1 = formatDate
-        elif queryType == QUERY_PROJECT:
-            fmt1 = lambda x: x
-        elif queryType == QUERY_DONE:
-            fmt1 = formatDate
-        else:
-            raise Exception()
-
-        tmpl = self.jinjaEnv.get_template("index.html")
-        html = tmpl.render(lst=lst, fmt1=fmt1)
-        baseUrl = QUrl.fromLocalFile(os.path.join(self.dataDir, "static/"))
-        self.ui.webView.setHtml(html, baseUrl)
-        self.ui.webView.page().mainFrame().addToJavaScriptWindowObject("qtWindow", self)
+            self.query.dueDate = dueDate
+        self.updateView()
 
     def updateViewAndKeepPosition(self):
         frame = self.ui.webView.page().currentFrame()
---
+++ queries.py
@@ -0,0 +1,152 @@
+# -*- coding: UTF-8 -*-
+from datetime import datetime
+
+from PyQt4.QtCore import QCoreApplication
+
+from sqlobject import AND, OR, LIKE, IN
+from sqlobject.sqlbuilder import Select
+
+from yokadi.db import Task, Project
+
+def formatDate(date):
+    """
+    strftime may return a string with accent ("August" in fr is "Août" for
+    example), so we need to turn it into proper unicode.
+    """
+    return unicode(date.strftime("%A %d %B %Y"), "utf-8")
+
+def __tr(txt):
+    return QCoreApplication.translate("", txt)
+
+def formatDueDate(dueDate):
+    today = datetime.now()
+    remaining = (dueDate.date() - today.date()).days
+    if remaining < 0:
+        txt = __tr("%1 days overdue").arg(-remaining)
+    elif remaining == 0:
+        txt = __tr("Due today")
+    elif remaining == 1:
+        txt = __tr("Due tomorrow")
+    elif remaining < 7:
+        txt = __tr("%1 days left").arg(remaining)
+    else:
+        txt = __tr("%1 weeks left").arg(remaining / 7)
+    return unicode(txt)
+
+def dueDateCssClass(task):
+    done = task.status == "done"
+    if done:
+        refDate = task.doneDate
+    else:
+        refDate = datetime.now()
+    remaining = (task.dueDate.date() - refDate.date()).days
+
+    if done:
+        if remaining < 0:
+            return "due-date-overdue"
+        else:
+            return ""
+
+    if remaining < 0:
+        return "due-date-overdue"
+    elif remaining == 0:
+        return "due-date-today"
+    elif remaining < 7:
+        return "due-date-week"
+    else:
+        return ""
+
+class Item(object):
+    __slots__ = ["task", "grp1", "grp2"]
+
+class Query(object):
+    __slots__ = ["projectName", "_filters"]
+    def __init__(self):
+        self.projectName = None
+        self._filters = []
+
+    def _addProjectFilter(self):
+        if self.projectName is None:
+            return
+        self._filters.append(
+            IN(
+                Task.q.project,
+                Select(Project.q.id, LIKE(Project.q.name, "%" + self.projectName + "%"))
+            ))
+
+    def run(self):
+        self._filters = []
+        self._addProjectFilter()
+
+class DueQuery(Query):
+    templateName = "index.html"
+    __slots__ = ["dueDate"]
+
+    def __init__(self):
+        super(DueQuery, self).__init__()
+        self.dueDate = None
+
+    def run(self):
+        super(DueQuery, self).run()
+        self._filters.append(OR(Task.q.status == "new", Task.q.status == "started"))
+        self._filters.append(Task.q.dueDate < self.dueDate)
+        tasks = Task.select(AND(*self._filters))
+
+        lst = []
+        for task in tasks:
+            item = Item()
+            item.task = task
+            item.grp1 = task.dueDate.date()
+            item.grp2 = task.project.name
+            lst.append(item)
+
+        fmt1 = formatDate
+        return dict(lst=lst, fmt1=fmt1)
+
+class ProjectQuery(Query):
+    templateName = "index.html"
+    def run(self):
+        super(ProjectQuery, self).run()
+        self._filters.append(OR(Task.q.status == "new", Task.q.status == "started"))
+        tasks = Task.select(AND(*self._filters))
+
+        lst = []
+        for task in tasks:
+            item = Item()
+            item.task = task
+            item.grp1 = task.project.name
+            item.grp2 = ""
+            lst.append(item)
+
+        fmt1 = lambda x: x
+        return dict(lst=lst, fmt1=fmt1)
+
+class DoneQuery(Query):
+    templateName = "index.html"
+    __slots__ = ["minDate", "maxDate"]
+
+    def  __init__(self):
+        super(DoneQuery, self).__init__()
+        self.minDate = None
+        self.maxDate = None
+
+    def run(self):
+        super(DoneQuery, self).run()
+        self._filters.append(Task.q.status == "done")
+        if self.minDate is not None:
+            self._filters.append(Task.q.doneDate >= self.minDate)
+        if self.maxDate is not None:
+            self._filters.append(Task.q.doneDate < self.maxDate)
+        tasks = Task.select(AND(*self._filters))
+
+        lst = []
+        for task in tasks:
+            item = Item()
+            item.task = task
+            item.grp1 = task.doneDate.date()
+            item.grp2 = task.project.name
+            lst.append(item)
+
+        fmt1 = formatDate
+        return dict(lst=lst, fmt1=fmt1)
+
