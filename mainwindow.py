007acd1650180e2c8cbd736467d69625cd393b4b
Aurélien Gâteau <agateau@kde.org>
2012-01-17 23:35:36 +0100

    Better implementation to print js console output

--- mainwindow.py
+++ mainwindow.py
@@ -16,7 +16,7 @@ from ui_mainwindow import Ui_MainWindow
 from addtaskdialog import AddTaskDialog
 from csspalette import CssPalette
 from qydateutils import datetimeFromQDate, qdateFromDatetime
-from console import Console
+from webpage import WebPage
 
 import queries
 
@@ -38,7 +38,6 @@ class MainWindow(QMainWindow):
 
         self.setupJinjaEnv()
         self.setupFilterWidgets()
-        self.setupConsole()
 
         self.ui.webView.settings().setDefaultTextEncoding("utf-8")
         self.ui.webView.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
@@ -57,10 +56,6 @@ class MainWindow(QMainWindow):
 
         self.updateFilterWidgets()
         self.updateQuery()
-
-    def setupConsole(self):
-        logging.basicConfig(level=logging.DEBUG)
-        self.console = Console(parent=self)
 
     def setupQueryListWidget(self):
         # Set widget width to be just a little wider than what is necessary to
@@ -126,9 +121,10 @@ class MainWindow(QMainWindow):
         # baseUrl must end with a trailing '/' otherwise QWebView won't be able
         # to load files from there
         baseUrl = QUrl.fromLocalFile(os.path.join(self.dataDir, "static/"))
+        page = WebPage(logger=None, parent=self)
+        page.mainFrame().addToJavaScriptWindowObject("qtWindow", self)
+        self.ui.webView.setPage(page)
         self.ui.webView.setHtml(html, baseUrl)
-        self.ui.webView.page().mainFrame().addToJavaScriptWindowObject("qtWindow", self)
-        self.ui.webView.page().mainFrame().addToJavaScriptWindowObject("console", self.console)
 
     def updateQuery(self):
         queryType = self.ui.queryListWidget.currentRow()
